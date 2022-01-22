d5912f0ef272100187d3b3df7d5c4631d759ea14
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2022-01-22 16:31:24 +0200

    Add doc texts

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,9 @@
+//! Old Norwegian/Norse Dictionary for Rust.
+//! 
+//! The dictionary consists of 40 000+ Old Norse words with Norwegian translations.
+//! 
+//! Based on "Dictionary of the Old Norwegian Language".
+
 mod reader;
 mod dictionary;
 pub use dictionary::{get_dictionary, DictionaryEntry};
